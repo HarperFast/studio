bfee9d1463823fd8411e483061559b19765f1509
Austin Akers <austin@harperdb.io>
2025-06-04 12:44:43 -0700

    updated config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+	build: {
+		outDir: 'web',
+		emptyOutDir: true,
+	},
+});
