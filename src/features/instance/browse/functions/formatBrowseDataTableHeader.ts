42f7ef64b0e35511e89a6ba4b6d5de9c2d43a4e1
Dawson Toth <dawson@harperdb.io>
2025-06-25 17:25:18 -0400

    Order columns by sortability

--- src/features/instance/browse/functions/formatBrowseDataTableHeader.ts
+++ src/features/instance/browse/functions/formatBrowseDataTableHeader.ts
@@ -5,23 +5,30 @@ function formatBrowseDataTableHeader(describeTableData: DescribeTableDataRespons
 	dataTableColumns: Array<ColumnDef<Record<string, unknown>>>; hash_attribute: string;
 } {
 	const { hash_attribute, attributes } = describeTableData;
-	const dataTableColumns: ColumnDef<Record<string, unknown>>[] = [];
+	const primaryKeyColumns: ColumnDef<Record<string, unknown>>[] = [];
+	const sortableColumns: ColumnDef<Record<string, unknown>>[] = [];
+	const normalColumns: ColumnDef<Record<string, unknown>>[] = [];
+	const timeColumns: ColumnDef<Record<string, unknown>>[] = [];
 	for (let i = attributes.length - 1; i >= 0; i--) {
 		const { attribute, is_primary_key, indexed } = attributes[i];
 
 		const dataTableColumn: ColumnDef<Record<string, unknown>> = {
-			header: attribute === 'id' ? 'Primary Key' : attribute,
+			header: is_primary_key ? 'Primary Key' : attribute,
 			accessorKey: attribute,
 			enableSorting: Boolean(is_primary_key || indexed),
 		};
-		if (attribute === '__createdtime__' || attribute === '__updatedtime__') {
-			dataTableColumns.push(dataTableColumn);
+		if (is_primary_key) {
+			primaryKeyColumns.push(dataTableColumn);
+		} else if (attribute === '__createdtime__' || attribute === '__updatedtime__') {
+			timeColumns.push(dataTableColumn);
+		} else if (dataTableColumn.enableSorting) {
+			sortableColumns.push(dataTableColumn);
 		} else {
-			dataTableColumns.unshift(dataTableColumn);
+			normalColumns.push(dataTableColumn);
 		}
 	}
 	return {
-		dataTableColumns,
+		dataTableColumns: [...primaryKeyColumns, ...sortableColumns, ...normalColumns, ...timeColumns],
 		hash_attribute,
 	};
 }
