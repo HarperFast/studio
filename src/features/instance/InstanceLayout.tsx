782dceb86a630a21bfa88812c114a2e2e46c4f1f
Dawson Toth <dawson@harperdb.io>
2025-07-01 12:04:28 -0400

    Fix race condition when setting instanceClient baseURL

--- src/features/instance/InstanceLayout.tsx
+++ src/features/instance/InstanceLayout.tsx
@@ -1,27 +1,17 @@
-import { instanceClient } from '@/config/instanceClient';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getInstanceInfoQueryOptions } from '@/features/instance/queries/getInstanceInfoQuery';
-import { Outlet, getRouteApi } from '@tanstack/react-router';
+import { getRouteApi, Outlet } from '@tanstack/react-router';
 import { InstanceNavBar } from '@/features/instance/InstanceNavBar';
-import { isLocalStudio } from '@/config/constants';
-import { useEffect } from 'react';
 
 const route = getRouteApi('');
 
 export function InstanceLayout() {
 	const { instanceId } = route.useParams();
-	const { data: instanceInfo, isSuccess } = useSuspenseQuery(getInstanceInfoQueryOptions(instanceId));
+	const { isSuccess } = useSuspenseQuery(getInstanceInfoQueryOptions(instanceId));
 
 	if (!isSuccess) {
 		throw new Error('Instance info not found');
 	}
-	useEffect(() => {
-		if (!isLocalStudio && instanceInfo) {
-			// Set the base URL for the instance client to the first FQDN of the instance
-			// This allows all subsequent API calls to use the correct base URL for the instance
-			instanceClient.defaults.baseURL = instanceInfo.instanceFqdn;
-		}
-	}, [instanceInfo]);
 	return (
 		<>
 			<nav className="fixed top-20 w-full h-12 z-39 px-4 md:px-12 bg-grey-700">
--- src/features/instance/queries/getInstanceInfoQuery.ts
+++ src/features/instance/queries/getInstanceInfoQuery.ts
@@ -2,6 +2,7 @@ import { apiClient } from '@/config/apiClient';
 import { queryKeys } from '@/react-query/constants';
 import { queryOptions } from '@tanstack/react-query';
 import { isLocalStudio } from '@/config/constants';
+import { instanceClient } from '@/config/instanceClient';
 
 type Cluster = {
 	id: string;
@@ -38,11 +39,16 @@ type Instance = {
 };
 
 const getInstanceInfo = async (instanceId: string) => {
-	// TODO: Work through any disagreements between the SchemaInstanceType and our local types here.
 	if (isLocalStudio) {
 		return null;
 	}
+	// TODO: Work through any disagreements between the SchemaInstanceType and our local types here.
 	const { data } = await apiClient.get(`/HDBInstance/${instanceId}` as '/HDBInstance/{id}');
+	if (data) {
+		// Set the base URL for the instance client to the first FQDN of the instance
+		// This allows all subsequent API calls to use the correct base URL for the instance
+		instanceClient.defaults.baseURL = data.instanceFqdn;
+	}
 	return data as Instance;
 };
 
