ea53a238b77e8b5cea2363091c77e5fd826a6a43
Austin Akers <austin@harperdb.io>
2025-07-17 12:57:23 -0700

    updated instance config overview page for local studio and cloud studio

---
+++ src/features/instance/config/overview/components/InstanceNodeName.tsx
@@ -0,0 +1,16 @@
+import { TextLoadingSkeleton } from '@/components/TextLoadingSkeleton';
+
+export const InstanceNodeName = ({
+	loadingInstanceInfo,
+	instanceInfo,
+}: {
+	loadingInstanceInfo?: boolean;
+	instanceInfo?: { name: string } | undefined;
+}) => {
+	return (
+		<>
+			<dt className="font-bold text-sm/6">Instance Node Name (for clustering)</dt>
+			<dd className="text-sm/6 sm:mt-2">{loadingInstanceInfo ? <TextLoadingSkeleton /> : instanceInfo?.name}</dd>
+		</>
+	);
+};
--- src/features/instance/config/overview/index.tsx
+++ src/features/instance/config/overview/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { ReactNode, useState } from 'react';
 import { useParams } from '@tanstack/react-router';
 import { toast } from 'sonner';
 import { getRegistrationInfoQueryOptions } from '@/features/instance/operations/queries/getRegistrationInfo';
@@ -6,12 +6,26 @@ import { useSuspenseQuery } from '@tanstack/react-query';
 import { getConfigurationQueryOptions } from '@/features/instance/operations/queries/getConfiguration';
 import Editor from '@monaco-editor/react';
 import { Button } from '@/components/ui/button';
-import { RemoveInstanceModal } from '../../modals/RemoveInstanceModal';
+import { RemoveInstanceModal } from '@/features/instance/modals/RemoveInstanceModal.tsx';
 import { TextLoadingSkeleton } from '@/components/TextLoadingSkeleton';
 import { useDeleteInstance } from '@/features/cluster/hooks/useDeleteInstance';
 import { getInstanceInfoQueryOptions } from '@/features/instance/operations/queries/getInstanceInfoQuery';
 import { useHumanFileSize } from '@/hooks/useHumanFileSize';
-import { useUpdateRestartInstance } from '../../operations/mutations/updateRestartInstance';
+import { useUpdateRestartInstance } from '@/features/instance/operations/mutations/updateRestartInstance.ts';
+import { isLocalStudio } from '@/config/constants';
+import { InstanceRAM } from '@/features/instance/config/overview/components/InstanceRAM';
+import { InstanceURL } from '@/features/instance/config/overview/components/InstanceURL';
+import { ApplicationURL } from '@/features/instance/config/overview/components/ApplicationURL';
+import { InstanceNodeName } from '@/features/instance/config/overview/components/InstanceNodeName';
+import { HarperVersion } from '@/features/instance/config/overview/components/HarperVersion';
+
+const LocalStudioOverview = ({ children }: { children: ReactNode }) => {
+	return <>{children}</>;
+};
+
+const CloudStudioOverview = ({ children }: { children: ReactNode }) => {
+	return <>{children}</>;
+};
 
 export function ConfigOverviewIndex() {
 	const { clusterId, instanceId } = useParams({ strict: false });
@@ -98,65 +112,67 @@ export function ConfigOverviewIndex() {
 
 	return (
 		<div className="h-full flex flex-col">
-			<dl className="flex-none grid grid-cols-1 sm:grid-cols-3">
-				<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
-					<dt className="font-bold text-sm/6">Instance URL</dt>
-					<dd className="text-sm/6 sm:mt-2">
-						{loadingInstanceInfo ? <TextLoadingSkeleton /> : instanceInfo?.instanceFqdn}
-					</dd>
-				</div>
-				<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
-					<dt className="font-bold text-sm/6">Application URL</dt>
-					<dd className="text-sm/6 sm:mt-2">
-						{loadingInstanceInfo ? <TextLoadingSkeleton /> : clusterInfo?.fqdn}
-					</dd>
-				</div>
-				<div className="px-4 pb-4 text-right sm:col-span-1 sm:px-0">
-					<Button
-						variant="destructiveOutline"
-						className="rounded-full cursor-pointer"
-						onClick={() => setIsRemoveInstanceModalOpen(true)}
-					>
-						Remove Instance
-					</Button>
-					<Button
-						variant="positiveOutline"
-						className="ml-4 rounded-full cursor-pointer"
-						onClick={restartingInstance}
-						disabled={isRestartInstancePending}
-					>
-						Restart Instance
-					</Button>
-				</div>
-				<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
-					<dt className="font-bold text-sm/6">Instance Node Name (for clustering)</dt>
-					<dd className="text-sm/6 sm:mt-2">{loadingInstanceInfo ? <TextLoadingSkeleton /> : instanceInfo?.name}</dd>
-				</div>
-				{/*<div className="px-4 pb-4 sm:col-span-1 sm:px-0">*/}
-				{/*	<dt className="font-bold text-sm/6">Created</dt>*/}
-				{/*	<dd className="text-sm/6 sm:mt-2">*/}
-				{/*		<TextLoadingSkeleton />*/}
-				{/*	</dd>*/}
-				{/*</div>*/}
-				{/*<div className="px-4 pb-4 sm:col-span-1 sm:px-0">*/}
-				{/*	<dt className="font-bold text-sm/6">Total Price</dt>*/}
-				{/*	<dd className="text-sm/6 sm:mt-2">*/}
-				{/*		<TextLoadingSkeleton className="w-10" />*/}
-				{/*	</dd>*/}
-				{/*</div>*/}
-				<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
-					<dt className="font-bold text-sm/6">Harper Version</dt>
-					<dd className="text-sm/6 sm:mt-2">
-						{loadingRegistration ? <TextLoadingSkeleton className="w-10" /> : registrationInfo.version}
-					</dd>
-				</div>
-				<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
-					<dt className="font-bold text-sm/6">RAM</dt>
-					<dd className="text-sm/6 sm:mt-2">
-						{loadingRegistration ? <TextLoadingSkeleton className="w-10" /> : ramAllocation}
-					</dd>
-				</div>
-			</dl>
+			{isLocalStudio ? (
+				<LocalStudioOverview>
+					<dl className="grid grid-cols-1 sm:grid-cols-3">
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<HarperVersion loadingRegistration={loadingRegistration} registrationInfo={registrationInfo} />
+						</div>
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<InstanceRAM loadingRegistration={loadingRegistration} ramAllocation={ramAllocation} />
+						</div>
+						<div className="px-4 pb-4 text-right sm:col-span-1 sm:px-0">
+							<Button
+								variant="positiveOutline"
+								className="ml-4 rounded-full cursor-pointer"
+								onClick={restartingInstance}
+								disabled={isRestartInstancePending}
+							>
+								Restart Instance
+							</Button>
+						</div>
+					</dl>
+				</LocalStudioOverview>
+			) : (
+				<CloudStudioOverview>
+					<dl className="flex-none grid grid-cols-1 sm:grid-cols-3">
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<InstanceURL loadingInstanceInfo={loadingInstanceInfo} clusterInfo={clusterInfo} />
+						</div>
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<ApplicationURL loadingInstanceInfo={loadingInstanceInfo} clusterInfo={clusterInfo} />
+						</div>
+						<div className="px-4 pb-4 text-right sm:col-span-1 sm:px-0">
+							<Button
+								variant="destructiveOutline"
+								className="rounded-full cursor-pointer"
+								onClick={() => setIsRemoveInstanceModalOpen && setIsRemoveInstanceModalOpen(true)}
+							>
+								Remove Instance
+							</Button>
+							<Button
+								variant="positiveOutline"
+								className="ml-4 rounded-full cursor-pointer"
+								onClick={restartingInstance}
+								disabled={isRestartInstancePending}
+							>
+								Restart Instance
+							</Button>
+						</div>
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<InstanceNodeName loadingInstanceInfo={loadingInstanceInfo} instanceInfo={instanceInfo} />
+						</div>
+
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<HarperVersion loadingRegistration={loadingRegistration} registrationInfo={registrationInfo} />
+						</div>
+						<div className="px-4 pb-4 sm:col-span-1 sm:px-0">
+							<InstanceRAM loadingRegistration={loadingRegistration} ramAllocation={ramAllocation} />
+						</div>
+					</dl>
+				</CloudStudioOverview>
+			)}
+
 			<h3 className="flex-none font-bold text-sm/6">Instance Config (read only)</h3>
 			<div className="grow">
 				{!loadingConfig ? (
