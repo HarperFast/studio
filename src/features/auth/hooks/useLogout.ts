7f22d88a3b5a1ef4556875fbbf68ef4b80523903
Dawson Toth <dawson@harperdb.io>
2025-07-31 09:51:54 -0400

    Sign out from all instances at once
    https://harperdb.atlassian.net/browse/STUDIO-226

--- src/features/auth/hooks/useLogout.ts
+++ src/features/auth/hooks/useLogout.ts
@@ -1,8 +1,10 @@
 import { apiClient } from '@/config/apiClient';
+import { authStore } from '@/lib/authStore';
 import { useMutation } from '@tanstack/react-query';
 import { logoutOnSuccess } from '@/features/auth/handlers/logoutOnSuccess';
 
 export async function onLogoutSubmit() {
+	await authStore.signOutFromPotentiallyAuthenticatedInstances();
 	await apiClient.post('/Logout/');
 }
 
