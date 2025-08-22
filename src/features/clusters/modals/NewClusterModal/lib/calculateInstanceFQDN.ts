a5e764a609a142fdc229d1bdc449e3b43df57ab3
Dawson Toth <dawson@harperdb.io>
2025-08-22 14:49:46 -0400

    Map 127.0.0.1 to localhost
    This will help with duplicate instance specifications
    
    https://harperdb.atlassian.net/browse/STUDIO-256

--- src/features/clusters/modals/NewClusterModal/lib/calculateInstanceFQDN.ts
+++ src/features/clusters/modals/NewClusterModal/lib/calculateInstanceFQDN.ts
@@ -3,7 +3,18 @@ import { NewClusterSchema } from '@/features/clusters/modals/NewClusterModal/new
 import { z } from 'zod';
 
 export function calculateInstanceFQDN(instance: z.infer<typeof NewClusterSchema.shape.instances.element>) {
-	const { secure, fqdn } = instance;
-	const port = instance.port || defaultOperationsApiPort;
+	let { secure, fqdn, port } = instance;
+	if (!port) {
+		port = defaultOperationsApiPort;
+	}
+	if (fqdn === '127.0.0.1') {
+		fqdn = 'localhost';
+	}
+	if (port === 443 && secure === 'false') {
+		secure = 'true';
+	}
+	if (port === 80 && secure === 'true') {
+		secure = 'false';
+	}
 	return `${secure === 'true' ? 'https' : 'http'}://${fqdn}${port === 443 || port === 80 ? '' : (':' + port)}`.toLowerCase();
 }
