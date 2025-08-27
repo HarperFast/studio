53ccb54a42d83a6ec76c506df94724df5869f365
Dawson Toth <dawson@harperdb.io>
2025-08-27 16:41:28 -0400

    Support smaller viewport on resources per instance and tweak spacing

--- src/features/clusters/upsert/components/ResourcesPerInstance.tsx
+++ src/features/clusters/upsert/components/ResourcesPerInstance.tsx
@@ -87,7 +87,7 @@ export function ResourcesPerInstance({ planLimits, resourcesPerInstance }: {
 		},
 	].filter(excludeFalsy), [planLimits, resourcesPerInstance]);
 
-	return <FormItem className="md:col-span-6">
+	return <FormItem className="col-span-3 md:col-span-6">
 		<FormLabel className="inline-flex">
 			<Button
 				type="button"
@@ -95,8 +95,8 @@ export function ResourcesPerInstance({ planLimits, resourcesPerInstance }: {
 				className="text-white cursor-pointer"
 				onClick={onUsageLimitsClick}
 			>
-				Purchasing usage block for {humanNumber(planLimits.readsPerMinuteCount!)} reads/min &amp;
-				{humanNumber(planLimits.totalReadCount)} total reads per region,<br/>
+				Purchasing usage block for {humanNumber(planLimits.readsPerMinuteCount!)} reads/min
+				&amp; {humanNumber(planLimits.totalReadCount)} total reads per region,<br/>
 				{humanNumber(planLimits.writesPerMinuteCount!)} writes/min &amp; {humanNumber(planLimits.totalWriteCount!)} total writes, for 3 months, expand for more details:
 				{toggled ? <ArrowDownIcon /> : <ArrowRightIcon />}
 			</Button>
