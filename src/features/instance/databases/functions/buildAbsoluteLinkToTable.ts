1f95409ccc0ba3841362c2d5b3f7221118277ff5
Dawson Toth <dawson@harperdb.io>
2025-09-05 11:42:06 -0400

    fix: Maintain selected instanceId when building link to table
    https://harperdb.atlassian.net/browse/STUDIO-384

--- src/features/instance/databases/functions/buildAbsoluteLinkToTable.ts
+++ src/features/instance/databases/functions/buildAbsoluteLinkToTable.ts
@@ -3,17 +3,26 @@ import { excludeFalsy } from '@/lib/arrays/excludeFalsy';
 export function buildAbsoluteLinkToTable({
 	clusterId,
 	databaseName,
+	instanceId,
 	organizationId,
 	tableName,
 }: {
 	clusterId?: string;
 	databaseName?: string;
+	instanceId?: string;
 	organizationId?: string;
 	tableName?: string;
 }): string {
 	const databases = [`databases`, databaseName, tableName].filter(excludeFalsy).join('/');
 	if (organizationId) {
-		return `/orgs/${organizationId}/${clusterId}/${databases}`;
-	}
+		return '/' + [
+			'orgs',
+			organizationId,
+			clusterId,
+			instanceId && 'instance',
+			instanceId,
+			databases,
+		].filter(excludeFalsy).join('/')
+;	}
 	return `/${databases}`;
 }
