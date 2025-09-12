8d950ae5bfbd6b178fb1354301638d3321e18571
Dawson Toth <dawson@harperdb.io>
2025-09-12 14:27:44 -0400

    feat: Show a toast during cluster saving
    https://harperdb.atlassian.net/browse/STUDIO-422

--- src/features/clusters/hooks/useUpdateCluster.ts
+++ src/features/clusters/hooks/useUpdateCluster.ts
@@ -2,12 +2,14 @@ import { apiClient } from '@/config/apiClient';
 import { SchemaCluster, SchemaClusterUpsert } from '@/lib/api.gen';
 import { useMutation } from '@tanstack/react-query';
 
-export async function onEditClusterSubmit(
+async function onEditClusterSubmit(
 	clusterInfo: Pick<SchemaCluster, 'id'> & Pick<SchemaClusterUpsert, 'regionPlans'>,
 ): Promise<SchemaCluster> {
-	const { data } = await apiClient.put(`/Cluster/${clusterInfo.id}` as '/Cluster/{id}', {
-		regionPlans: clusterInfo.regionPlans,
-	});
+	const { data } = await apiClient.put(
+		`/Cluster/${clusterInfo.id}` as '/Cluster/{id}',
+		{ regionPlans: clusterInfo.regionPlans },
+		{ timeout: 120_000 },
+	);
 	return data;
 }
 
