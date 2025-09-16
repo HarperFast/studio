af5aaaf8f047286c6e7f02840b7a2aebe5de1676
Dawson Toth <dawson@harperdb.io>
2025-09-16 13:26:53 -0400

    feat: Order logs desc by default
    https://harperdb.atlassian.net/browse/STUDIO-407

--- src/features/instance/log/index.tsx
+++ src/features/instance/log/index.tsx
@@ -27,14 +27,12 @@ type RowData = {
 	original: ReadLogItem;
 };
 
-
-
 const defaultFormValues: z.infer<typeof LogFiltersFormSchema> = {
 	limit: '100',
 	level: 'undefined',
 	from: '',
 	until: '',
-	order: 'asc',
+	order: 'desc',
 };
 
 const columns: ColumnDef<ReadLogItem>[] = [
@@ -116,13 +114,7 @@ const isValidDateRange = (startDate?: string, endDate?: string) => {
 };
 
 export function Logs() {
-	const [logFilters, setLogFilters] = useState<z.infer<typeof LogFiltersFormSchema>>({
-		limit: '100',
-		level: 'undefined',
-		from: '',
-		until: '',
-		order: 'asc',
-	});
+	const [logFilters, setLogFilters] = useState<z.infer<typeof LogFiltersFormSchema>>(defaultFormValues);
 
 	const [isViewLogModalOpen, setIsViewLogModalOpen] = useState(false);
 	const [selectedLogData, setSelectedLogData] = useState<ReadLogItem | undefined>();
