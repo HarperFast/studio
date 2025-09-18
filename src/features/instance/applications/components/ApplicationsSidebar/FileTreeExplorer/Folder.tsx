01aa875b2e4962b0f1b0e8add7c3e53ac09719c0
Dawson Toth <dawson@harperdb.io>
2025-09-18 12:50:37 -0400

    feat: Remember folder open state in session storage
    https://harperdb.atlassian.net/browse/STUDIO-386

--- src/features/instance/applications/components/ApplicationsSidebar/FileTreeExplorer/Folder.tsx
+++ src/features/instance/applications/components/ApplicationsSidebar/FileTreeExplorer/Folder.tsx
@@ -1,16 +1,18 @@
 import { DirectoryEntry } from '@/features/instance/operations/queries/getComponents';
-import { useState } from 'react';
+import { useSessionStorage } from '@/hooks/useSessionStorage';
+import { useParams } from '@tanstack/react-router';
+import { File } from './File';
 import { calculateIconForDirectoryEntry } from './lib/calculateIconForDirectoryEntry';
 import { directorySortComparator } from './lib/directorySortComparator';
 import { parseFileExtension } from './lib/parseFileExtension';
-import { File } from './File';
 
 export function Folder({ directoryEntry, depth, pkg }: {
 	readonly directoryEntry: DirectoryEntry;
 	readonly depth: number;
 	readonly pkg?: string;
 }) {
-	const [open, setOpen] = useState(depth <= 1 && !directoryEntry.package);
+	const { instanceId, clusterId } = useParams({ strict: false });
+	const [open, setOpen] = useSessionStorage(`FolderOpened/${instanceId ?? clusterId}/${directoryEntry.key}` as 'FolderOpened/{instanceId}/{key}', depth <= 1 && !directoryEntry.package);
 
 	const entries = [...(directoryEntry.entries || [])].sort(directorySortComparator);
 	const fileExtension = parseFileExtension(directoryEntry.name);
