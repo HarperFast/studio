c03d2711d1746432332be9aa43114ebb3c21ec70
Dawson Toth <dawson@harperdb.io>
2025-10-07 15:46:12 -0400

    feat: Use updated logo in the cloud
    https://harperdb.atlassian.net/browse/STUDIO-476

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -266,7 +266,11 @@ function AnonymousNav() {
 function Logo() {
 	return (
 		<>
-			<img src="/logo_harper_db_studio.png" alt="logo" className="w-64 hidden md:inline-block" />
+			{isLocalStudio ? (
+				<img src="/harper-studio_white.png" alt="Harper Studio" className="w-64 hidden md:inline-block" />
+			) : (
+				<img src="/harper-fabric_white.png" alt="Harper Fabric" className="w-64 hidden md:inline-block" />
+			)}
 			<img src="/HDBDogOnly.svg" width="50px" height="44px" alt="Harper" className="inline-block md:hidden" />
 		</>
 	);
