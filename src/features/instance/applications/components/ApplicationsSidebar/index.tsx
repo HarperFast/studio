f7aeb714313377caa5eafe2bbd67b86d3797db67
Dawson Toth <dawson@harperdb.io>
2025-10-07 16:03:46 -0400

    fix: Lock down status check and file instance actions a bit more
    https://harperdb.atlassian.net/browse/STUDIO-467

--- src/features/instance/applications/components/ApplicationsSidebar/index.tsx
+++ src/features/instance/applications/components/ApplicationsSidebar/index.tsx
@@ -1,11 +1,13 @@
 import { GetComponentsResponse } from '@/features/instance/operations/queries/getComponents';
+import { useInstanceBrowseManagePermission } from '@/hooks/usePermissions';
 import { FileTreeExplorer } from './FileTreeExplorer';
 import { FileMenuActionButtons } from './FileTreeExplorer/FileMenuActionButtons';
 
 export function ApplicationsSidebar({ fileTreeQueryData }: { fileTreeQueryData: GetComponentsResponse }) {
+	const canManageBrowseInstance = useInstanceBrowseManagePermission();
 	return (
 		<>
-			<FileMenuActionButtons />
+			{canManageBrowseInstance && (<FileMenuActionButtons />)}
 			<FileTreeExplorer files={fileTreeQueryData} />
 		</>
 	);
