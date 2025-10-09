3356ef26e0a40110eab56841adf8bd814b528884
Dawson Toth <dawson@harperdb.io>
2025-10-09 13:00:54 -0400

    chore: Add .editorconfig and `VITE_HOST` example

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,18 +3,18 @@ import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    environment: 'node',
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'lcov'],
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+	test: {
+		environment: 'node',
+		include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+		globals: true,
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'lcov'],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 });
