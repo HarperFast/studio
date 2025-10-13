39165654450fba4cf6609ca9e7ced830961dc809
Wes Morgan <git@wesmorgan.me>
2025-10-13 11:27:29 -0600

    chore: Use lib/units code to implement humanFileSize
    ...and update tests

--- src/lib/humanFileSize.test.ts
+++ src/lib/humanFileSize.test.ts
@@ -5,26 +5,27 @@ describe('humanFileSize', () => {
 	it('should return bytes for values less than 1024', () => {
 		expect(humanFileSize(500)).toBe('500 B');
 		expect(humanFileSize(0)).toBe('0 B');
-		expect(humanFileSize(1023)).toBe('1023 B');
+		expect(humanFileSize(1023)).toBe('1,023 B');
 	});
 
-	it('should convert to KB for values between 1024 and 1048575', () => {
-		expect(humanFileSize(1024)).toBe('1 KB');
-		expect(humanFileSize(2048)).toBe('2 KB');
-		expect(humanFileSize(1000000)).toBe('977 KB');
+	it('should convert to KiB for values between 1024 and 1048575', () => {
+		expect(humanFileSize(1024)).toBe('1 KiB');
+		expect(humanFileSize(2048)).toBe('2 KiB');
+		expect(humanFileSize(1000000)).toBe('977 KiB');
+		expect(humanFileSize(1023900)).toBe('1,000 KiB')
 	});
 
-	it('should convert to MB for appropriate values', () => {
-		expect(humanFileSize(1048576)).toBe('1 MB');
-		expect(humanFileSize(2097152)).toBe('2 MB');
+	it('should convert to MiB for appropriate values', () => {
+		expect(humanFileSize(1048576)).toBe('1 MiB');
+		expect(humanFileSize(2097152)).toBe('2 MiB');
 	});
 
-	it('should convert to GB for appropriate values', () => {
-		expect(humanFileSize(1073741824)).toBe('1 GB');
+	it('should convert to GiB for appropriate values', () => {
+		expect(humanFileSize(1073741824)).toBe('1 GiB');
 	});
 
 	it('should apply the multiplier correctly', () => {
-		expect(humanFileSize(2, 1024)).toBe('2 KB');
-		expect(humanFileSize(2, 1048576)).toBe('2 MB');
+		expect(humanFileSize(2, 1024)).toBe('2 KiB');
+		expect(humanFileSize(2, 1048576)).toBe('2 MiB');
 	});
 });
