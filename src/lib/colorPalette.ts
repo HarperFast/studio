e11324aeb5f34abc2f579e4084c38bf3d7560650
Wes Morgan <git@wesmorgan.me>
2025-10-13 11:25:28 -0600

    chore: Move harperColorPalette to lib

---
+++ src/lib/colorPalette.ts
@@ -0,0 +1,9 @@
+export const harperPalette = {
+	'persistence-purple': '#403B8A',
+	'b-tree-green': '#55C58F',
+	'cyber-grape': '#7A3A87',
+	'quantum-purple': '#312556',
+	'cloud-white': '#F5F5F5',
+	'acid-magenta': '#C63368',
+	'edge-gray': '#383D40',
+};
