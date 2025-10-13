683ebd3bd1896e235cfdfd173cca968c6d81b662
Wes Morgan <git@wesmorgan.me>
2025-10-13 11:26:22 -0600

    chore: Move units code to lib & add tests

---
+++ src/lib/units.ts
@@ -0,0 +1,56 @@
+export type Units = 'bytes' | 'secs';
+
+const dataUnits = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+const timeUnits = ['secs', 'mins', 'hrs'];
+const unitsMultipliers = {
+	bytes: 1024,
+	secs: 60,
+};
+
+export function scaleValueToUnits(value: number, baseUnits: string, units: string) {
+	let availableUnits;
+	let scaleBase;
+	switch (baseUnits) {
+		case 'bytes': {
+			availableUnits = dataUnits;
+			scaleBase = unitsMultipliers.bytes;
+			break;
+		}
+		case 'secs': {
+			availableUnits = timeUnits;
+			scaleBase = unitsMultipliers.secs;
+			break;
+		}
+		default:
+			return value;
+	}
+	const scaleExponent = availableUnits.indexOf(units);
+	if (scaleExponent === -1) {
+		return value;
+	}
+	const scaleFactor = scaleBase ** scaleExponent;
+	return value / scaleFactor;
+}
+
+export function determineUnits(baseUnits: string, value: number) {
+	let availableUnits;
+	let unitsMultiplier;
+	switch (baseUnits) {
+		case 'bytes':
+			availableUnits = dataUnits;
+			unitsMultiplier = unitsMultipliers.bytes;
+			break;
+		case 'secs':
+			availableUnits = timeUnits;
+			unitsMultiplier = unitsMultipliers.secs;
+			break;
+		default:
+			return baseUnits;
+	}
+	let i = 0;
+	while (value >= unitsMultiplier && i < availableUnits.length - 1) {
+		value /= unitsMultiplier;
+		i++;
+	}
+	return availableUnits[i];
+}
