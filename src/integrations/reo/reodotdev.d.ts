137c09188e29d79b4ee4cd060e5df46b8edc8055
Dawson Toth <dawson@harperdb.io>
2025-10-15 11:26:43 -0400

    fix: Try reoing differently

--- src/integrations/reo/reodotdev.d.ts
+++ src/integrations/reo/reodotdev.d.ts
@@ -1,9 +1,8 @@
 declare module 'reodotdev' {
-	export function loadReoScript({ clientID: string }): Promise<{
-		init: null | (({ clientID: string }) => ReoClient);
-	}>;
+	export function loadReoScript({ clientID: string }): Promise<ReoClient>;
 
 	export interface ReoClient {
+		init: null | (({ clientID: string }) => ReoClient);
 		identify: null | ((person: { username: string; type: string; company: string; }) => void);
 	}
 }
