08ac865129549097f5e54331e8f9628b2b5486bb
Dawson Toth <dawson@harperdb.io>
2025-10-15 11:26:43 -0400

    fix: Try reoing differently

--- src/integrations/reo/reo.ts
+++ src/integrations/reo/reo.ts
@@ -7,7 +7,10 @@ export function useReo() {
 	useEffect(() => {
 		if (import.meta.env.VITE_REO_DEV_CLIENT_ID && import.meta.env.VITE_REO_DEV_CLIENT_ID !== '0') {
 			loadReoScript({ clientID: import.meta.env.VITE_REO_DEV_CLIENT_ID })
-				.then((Reo) => reoClient = Reo?.init?.({ clientID: import.meta.env.VITE_REO_DEV_CLIENT_ID }));
+				.then((Reo) => {
+					Reo?.init?.({ clientID: import.meta.env.VITE_REO_DEV_CLIENT_ID });
+					return reoClient = Reo;
+				});
 		}
 	}, []);
 }
