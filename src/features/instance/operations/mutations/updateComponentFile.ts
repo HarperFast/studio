d5eb29cda8ca482eb8fae159fc13c8996f1fe2aa
Dawson Toth <dawson@harperdb.io>
2025-10-21 12:29:58 -0400

    feat: Replicate set component file
    https://harperdb.atlassian.net/browse/STUDIO-523

--- src/features/instance/operations/mutations/updateComponentFile.ts
+++ src/features/instance/operations/mutations/updateComponentFile.ts
@@ -1,18 +1,19 @@
-import { InstanceClientConfig } from '@/config/instanceClientConfig';
+import { InstanceClientConfig, InstanceTypeConfig } from '@/config/instanceClientConfig';
 import { useMutation } from '@tanstack/react-query';
 
-export interface SetComponentFileRequest extends InstanceClientConfig {
+export interface SetComponentFileRequest extends InstanceClientConfig, InstanceTypeConfig {
 	file: string;
 	payload?: string;
 	project: string;
 }
 
-async function onUpdateComponentFile({ file, payload, project, instanceClient }: SetComponentFileRequest) {
+async function onUpdateComponentFile({ file, payload, project, entityType, instanceClient }: SetComponentFileRequest) {
 	const { data } = await instanceClient.post('/', {
 		operation: 'set_component_file',
 		file,
 		payload,
 		project,
+		replicated: entityType === 'cluster',
 	});
 	return data;
 }
