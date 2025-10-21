aa5c96a8ab808b54a6bc336774424e7fca09f4d7
Dawson Toth <dawson@harperdb.io>
2025-10-21 12:21:08 -0400

    feat: Pasta

--- src/features/instance/applications/index.tsx
+++ src/features/instance/applications/index.tsx
@@ -1,3 +1,4 @@
+import { PanelLeftIcon } from 'lucide-react';
 import { ApplicationsSidebar } from './components/ApplicationsSidebar';
 import { TextEditorView } from './components/TextEditorView';
 import { EditorViewProvider } from './context/EditorViewProvider';
@@ -5,13 +6,24 @@ import { EditorViewProvider } from './context/EditorViewProvider';
 export function ApplicationsEditor() {
 	return (
 		<EditorViewProvider>
-			<div className="grid grid-cols-1 gap-4 md:grid-cols-12 h-[calc(100vh-theme(spacing.40))]">
-				<section className="col-span-1 overflow-y-auto min-h-48 text-white md:col-span-4 lg:col-span-3">
+			<button
+				data-drawer-target="default-sidebar"
+				data-drawer-toggle="default-sidebar"
+				aria-controls="default-sidebar"
+				type="button"
+				className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+				<span className="sr-only">Open sidebar</span>
+				<PanelLeftIcon />
+			</button>
+
+			<aside id="default-sidebar" className="pt-[calc(theme(spacing.32))] w-56 fixed top-0 left-0 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+				<div className="h-full overflow-y-auto">
 					<ApplicationsSidebar />
-				</section>
-				<section className="col-span-1 text-white md:col-span-8 lg:col-span-9">
-					<TextEditorView />
-				</section>
+				</div>
+			</aside>
+
+			<div className="sm:ml-48">
+				<TextEditorView />
 			</div>
 		</EditorViewProvider>
 	);
