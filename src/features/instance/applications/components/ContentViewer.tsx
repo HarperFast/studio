2a1368db36a8f7efdc94b1aee3d9d4e94eb1011a
Dawson Toth <dawson@harperdb.io>
2025-10-23 12:25:52 -0400

    feat: Embed new/import dialog
    https://harperdb.atlassian.net/browse/STUDIO-483

--- src/features/instance/applications/components/ContentViewer.tsx
+++ src/features/instance/applications/components/ContentViewer.tsx
@@ -1,11 +1,17 @@
 import { Loading } from '@/components/Loading';
+import { newApplication } from '@/features/instance/applications/components/ApplicationsSidebar/specialItems';
 import { TextEditorView } from '@/features/instance/applications/components/TextEditorView';
 import { isDirectory } from '@/features/instance/applications/context/isDirectory';
 import { useEditorView } from '@/features/instance/applications/hooks/useEditorView';
+import { CreateOrImportAnApplication } from '@/features/instance/applications/modals/CreateOrImportAnApplication';
 import Markdown from 'react-markdown';
 
 export function ContentViewer() {
 	const { openedEntry, openedEntryContents } = useEditorView();
+
+	if (openedEntry?.path === newApplication) {
+		return <CreateOrImportAnApplication />;
+	}
 
 	if (isDirectory(openedEntry)) {
 		if (openedEntryContents !== undefined) {
