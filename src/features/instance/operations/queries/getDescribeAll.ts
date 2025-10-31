e875d1a6b067899cd8f093ce80e02b29bfffe32b
Dawson Toth <dawson@harperdb.io>
2025-10-31 15:25:33 -0400

    fix: Remove timeout from table searches and add abort signals
    https://harperdb.atlassian.net/browse/STUDIO-566

--- src/features/instance/operations/queries/getDescribeAll.ts
+++ src/features/instance/operations/queries/getDescribeAll.ts
@@ -5,10 +5,10 @@ import { queryOptions } from '@tanstack/react-query';
 export function getDescribeAllQueryOptions({ entityId, instanceClient }: InstanceClientIdConfig) {
 	return queryOptions({
 		queryKey: [entityId, 'describe_all'] as const,
-		queryFn: async () => {
+		queryFn: async ({ signal }) => {
 			const { data } = await instanceClient.post<InstanceDatabaseMap>('/', {
 				operation: 'describe_all',
-			});
+			}, { signal });
 			return data;
 		},
 		staleTime: 60 * 1000,
