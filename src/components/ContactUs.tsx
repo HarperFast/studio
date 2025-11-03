409f786af980bf59409de1a3ad90f5b2676b7506
Dawson Toth <dawson@harperdb.io>
2025-11-03 14:47:21 -0500

    fix: Contact us through Discord
    https://harperdb.atlassian.net/browse/STUDIO-573

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -1,5 +1,5 @@
 export function ContactUs() {
-	return (<a href="https://www.harpersystems.dev/contact" target="_blank" className="underline">
+	return (<a href="https://discord.com/channels/1415002037439041710/1415002038286286994" target="_blank" className="underline">
 		Contact us
 	</a>);
 }
