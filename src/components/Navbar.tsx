25fe3175571770e6a25f58c0b70a0c606699be3f
Dawson Toth <dawson@harperdb.io>
2025-11-06 12:08:06 -0500

    fix: DRY and auto-collapse mobile nav on click
    https://harperdb.atlassian.net/browse/STUDIO-517
    https://harperdb.atlassian.net/browse/STUDIO-520

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { NavigationMenu } from '@/components/ui/navigation/NavigationMenu';
 import { NavigationMenuItem } from '@/components/ui/navigation/NavigationMenuItem';
 import { NavigationMenuLink } from '@/components/ui/navigation/NavigationMenuLink';
@@ -8,6 +7,7 @@ import { defaultInstanceRoute, isLocalStudio } from '@/config/constants';
 import { useLogoutMutation } from '@/features/auth/hooks/useLogout';
 import { useOverallAuth } from '@/hooks/useAuth';
 import { useOrganizationPermissions, useOrganizationRolePermissions } from '@/hooks/usePermissions';
+import { excludeFalsy } from '@/lib/arrays/excludeFalsy';
 import { getDefaultSignedInCloudRouteForUser } from '@/lib/urls/getDefaultSignedInCloudRouteForUser';
 import { Link, useNavigate, useParams, useRouter } from '@tanstack/react-router';
 import {
@@ -22,218 +22,117 @@ import {
 	UsersIcon,
 	X,
 } from 'lucide-react';
-import { useCallback, useState } from 'react';
+import { ReactNode, useCallback, useMemo, useState } from 'react';
 import { toast } from 'sonner';
 
 const activeLinkProps = { className: 'text-white' };
 
-function MobileNav({ signOut }: { signOut: () => void }) {
+export function Navbar() {
+	const { mutate: signOut } = useLogoutMutation();
+	const navigate = useNavigate();
 	const { user } = useOverallAuth();
-	const defaultCloudRoute = getDefaultSignedInCloudRouteForUser(user);
+	const router = useRouter();
 	const { organizationId }: { organizationId: string; } = useParams({ strict: false });
 	const { update: canUpdateOrganization } = useOrganizationPermissions(organizationId);
 	const { view: showOrgUsersAndRoles } = useOrganizationRolePermissions(organizationId);
 	const showBilling = canUpdateOrganization;
-	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const handleSignOut = useCallback(() => {
+		signOut(undefined, {
+			onSuccess: async () => {
+				toast.success('Success', {
+					description: 'You have been signed out successfully.',
+					action: {
+						label: 'Dismiss',
+						onClick: () => toast.dismiss(),
+					},
+				});
+				void navigate({ to: '/sign-in' });
+				void router.invalidate();
+			},
+		});
+	}, [signOut, router, navigate]);
+
+	const menuItems: Array<MenuGroup | MenuItem> = useMemo(
+		() => [
+			!isLocalStudio && {
+				to: '/',
+				icon: <BuildingIcon />,
+				text: 'Organizations',
+				textBreakpoint: 'xl',
+			},
+			!isLocalStudio && {
+				text: 'SubOrganizations',
+				items: [
+					showOrgUsersAndRoles && {
+						to: `/${organizationId}/roles`,
+						icon: <HandshakeIcon />,
+						text: 'Roles',
+						textBreakpoint: 'lg',
+					},
+					showOrgUsersAndRoles && {
+						to: `/${organizationId}/users`,
+						icon: <UsersIcon />,
+						text: 'Users',
+						textBreakpoint: 'lg',
+					},
+					showBilling && {
+						to: `/${organizationId}/billing`,
+						icon: <ReceiptIcon />,
+						text: 'Billing',
+						textBreakpoint: 'xl',
+					},
+				].filter(excludeFalsy),
+			},
+			!isLocalStudio && {
+				to: '/profile',
+				icon: <UserIcon />,
+				text: 'Profile',
+				textBreakpoint: 'xl',
+			},
+			{
+				to: 'https://docs.harperdb.io/docs',
+				target: '_blank',
+				icon: <BookOpenTextIcon />,
+				text: 'Docs',
+				textBreakpoint: isLocalStudio ? 'md' : 'xl',
+			},
+			{
+				onClick: handleSignOut,
+				icon: <LogOutIcon />,
+				text: 'Sign Out',
+				textBreakpoint: isLocalStudio ? 'md' : 'xl',
+			},
+		].filter(excludeFalsy) satisfies Array<MenuGroup | MenuItem>,
+		[organizationId, showBilling, showOrgUsersAndRoles, handleSignOut],
+	);
+
+	if (!user) {
+		return <AnonymousNav />;
+	}
 	return (
-		<div className="md:hidden" id="mobile-menu">
-			<div className="flex items-center justify-between">
-				<Link to={isLocalStudio ? defaultInstanceRoute : defaultCloudRoute}>
-					<Logo />
-					<Version />
-				</Link>
-				<button
-					type="button"
-					className="shadow-xs text-grey-400 hover:text-white hover:bg-black-dark"
-					onClick={() => {
-						setIsMenuOpen(!isMenuOpen);
-					}}
-				>
-					<span className="sr-only">{isMenuOpen ? 'Close menu' : 'Open menu'}</span>
-					{isMenuOpen ? <X /> : <Menu />}
-				</button>
-			</div>
-			<div
-				className={`${
-					isMenuOpen ? 'block' : 'hidden'
-				} md:hidden space-y-1 pb-3 bg-black-dark absolute left-0 top-full w-full rounded-b-md`}
-			>
-				{!isLocalStudio && (
-					<>
-						<Link
-							to="/"
-							className="flex flex-row px-3 py-2 text-base font-medium text-white bg-gray-900 rounded-md"
-							activeProps={activeLinkProps}
-						>
-							<BuildingIcon className="mr-4" /> Organizations
-						</Link>
-						{(showOrgUsersAndRoles || showBilling) ? (
-							<div className="bg-black rounded-2xl">
-								{showOrgUsersAndRoles && (<Link
-									to={`/${organizationId}/roles`}
-									className="flex flex-row px-3 py-2 text-base text-gray-300 font-medium rounded-md"
-									activeProps={activeLinkProps}
-								>
-									<HandshakeIcon className="mr-4" /> Roles
-								</Link>)}
-								{showOrgUsersAndRoles && (<Link
-									to={`/${organizationId}/users`}
-									className="flex flex-row px-3 py-2 text-base text-gray-300 font-medium rounded-md"
-									activeProps={activeLinkProps}
-								>
-									<UsersIcon className="mr-4" /> Users
-								</Link>)}
-								{showBilling && (<Link
-									to={`/${organizationId}/billing`}
-									className="flex flex-row px-3 py-2 text-base text-gray-300 font-medium rounded-md"
-									activeProps={activeLinkProps}
-								>
-									<ReceiptIcon className="mr-4" /> Billing
-								</Link>)}
-							</div>
-						) : (
-							''
-						)}
-					</>
-				)}
-				{!isLocalStudio && (
-					<Link
-						to="/profile"
-						className="flex flex-row items-center px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-					>
-						<UserIcon className="mr-4" />
-						Profile
-					</Link>
-				)}
-				<Link
-					to="https://docs.harperdb.io/docs"
-					target="_blank"
-					rel="noreferrer noopener"
-					className="flex flex-row px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-				>
-					<BookOpenTextIcon className="mr-4" /> Docs
-				</Link>
-				<Link
-					to={undefined}
-					onClick={signOut}
-					className="flex flex-row px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white"
-				>
-					<LogOutIcon className="mr-4" /> Sign Out
-				</Link>
-			</div>
-		</div>
+		<>
+			<MobileNav menuItems={menuItems} />
+			<DesktopNav menuItems={menuItems} />
+		</>
 	);
 }
 
-function DesktopNav({ signOut }: { signOut: () => void }) {
-	const { user } = useOverallAuth();
-	const defaultCloudRoute = getDefaultSignedInCloudRouteForUser(user);
-	const { organizationId }: { organizationId: string; } = useParams({ strict: false });
-	const { update: canUpdateOrganization } = useOrganizationPermissions(organizationId);
-	const { view: showOrgUsersAndRoles } = useOrganizationRolePermissions(organizationId);
-	const showBilling = canUpdateOrganization;
+interface MenuGroup {
+	text: string;
+	items: MenuItem[];
+}
 
-	return (
-		<div className="hidden md:block">
-			<div className="flex items-center justify-between">
-				<div className="inline-block">
-					<Link to={isLocalStudio ? defaultInstanceRoute : defaultCloudRoute}>
-						<Logo />
-						<Version />
-					</Link>
-				</div>
-				<NavigationMenu>
-					<NavigationMenuList className="text-grey-400">
-						{!isLocalStudio && (
-							<>
-								<NavigationMenuItem>
-									<NavigationMenuLink asChild>
-										<Link to="/" className="flex-row items-center" activeProps={activeLinkProps}>
-											<BuildingIcon />
-											<span className="hidden xl:inline-block">Organizations</span>
-										</Link>
-									</NavigationMenuLink>
-								</NavigationMenuItem>
-								{(showOrgUsersAndRoles || showBilling) ? (
-									<div className="bg-black rounded-2xl flex">
-										{showOrgUsersAndRoles && (<NavigationMenuItem>
-											<NavigationMenuLink asChild>
-												<Link
-													to={`/${organizationId}/roles`}
-													className="flex-row items-center"
-													activeProps={activeLinkProps}
-												>
-													<HandshakeIcon className="inline-block" />
-													<span className="hidden xl:inline-block">Roles</span>
-													<span className="xl:hidden">&nbsp;</span>
-												</Link>
-											</NavigationMenuLink>
-										</NavigationMenuItem>)}
-										{showOrgUsersAndRoles && (<NavigationMenuItem>
-											<NavigationMenuLink asChild>
-												<Link
-													to={`/${organizationId}/users`}
-													className="flex-row items-center"
-													activeProps={activeLinkProps}
-												>
-													<UsersIcon className="inline-block" />
-													<span className="hidden xl:inline-block">Users</span>
-													<span className="xl:hidden">&nbsp;</span>
-												</Link>
-											</NavigationMenuLink>
-										</NavigationMenuItem>)}
-										{showBilling && (<NavigationMenuItem>
-											<NavigationMenuLink asChild>
-												<Link
-													to={`/${organizationId}/billing`}
-													className="flex-row items-center"
-													activeProps={activeLinkProps}
-												>
-													<ReceiptIcon className="inline-block" />
-													<span className="hidden xl:inline-block">Billing</span>
-													<span className="xl:hidden">&nbsp;</span>
-												</Link>
-											</NavigationMenuLink>
-										</NavigationMenuItem>)}
-									</div>
-								) : (
-									''
-								)}
-							</>
-						)}
-						{!isLocalStudio && (
-							<NavigationMenuItem>
-								<NavigationMenuLink asChild>
-									<Link to="/profile" className="flex-row items-center" activeProps={activeLinkProps}>
-										<UserIcon /> <span className="hidden lg:inline-block">Profile</span>
-									</Link>
-								</NavigationMenuLink>
-							</NavigationMenuItem>
-						)}
-						<NavigationMenuItem>
-							<NavigationMenuLink asChild>
-								<Link
-									to="https://docs.harperdb.io/docs"
-									target="_blank"
-									rel="noreferrer noopener"
-									className="flex-row items-center"
-								>
-									<BookOpenTextIcon /> <span className="hidden lg:inline-block">Docs</span>
-								</Link>
-							</NavigationMenuLink>
-						</NavigationMenuItem>
-						<NavigationMenuItem>
-							<NavigationMenuLink asChild>
-								<Link to={undefined} onClick={signOut} className="flex-row items-center">
-									<LogOutIcon /> <span className="hidden lg:inline-block">Sign Out</span>
-								</Link>
-							</NavigationMenuLink>
-						</NavigationMenuItem>
-					</NavigationMenuList>
-				</NavigationMenu>
-			</div>
-		</div>
-	);
+interface MenuItem {
+	to?: string;
+	onClick?: () => void;
+	icon: ReactNode;
+	text: string;
+	textBreakpoint?: string;
+}
+
+function isMenuGroup(item: MenuGroup | MenuItem): item is MenuGroup {
+	return (item as MenuGroup).items !== undefined;
 }
 
 function AnonymousNav() {
@@ -272,6 +171,123 @@ function AnonymousNav() {
 	);
 }
 
+function DesktopNav({ menuItems }: { menuItems: Array<MenuGroup | MenuItem> }) {
+	const { user } = useOverallAuth();
+	const defaultCloudRoute = getDefaultSignedInCloudRouteForUser(user);
+
+	return (
+		<div className="hidden md:block">
+			<div className="flex items-center justify-between">
+				<div className="inline-block">
+					<Link to={isLocalStudio ? defaultInstanceRoute : defaultCloudRoute}>
+						<Logo />
+						<Version />
+					</Link>
+				</div>
+				<NavigationMenu>
+					<NavigationMenuList className="text-grey-400">
+						{menuItems.map(menuItem => isMenuGroup(menuItem)
+							? !!menuItem.items.length && (
+							<div key={menuItem.text} className="bg-black rounded-2xl flex">
+								{menuItem.items.map(innerMenuItem => (
+									<DesktopNavItem key={innerMenuItem.text} menuItem={innerMenuItem} />
+								))}
+							</div>
+						)
+							: (
+								<DesktopNavItem key={menuItem.text} menuItem={menuItem} />
+							))}
+					</NavigationMenuList>
+				</NavigationMenu>
+			</div>
+		</div>
+	);
+}
+
+function DesktopNavItem({ menuItem }: { menuItem: MenuItem }) {
+	return (
+		<NavigationMenuItem className="text-gray-400 hover:text-white">
+			<NavigationMenuLink asChild>
+				<Link
+					to={menuItem.to}
+					onClick={menuItem.onClick}
+					className="flex-row items-center"
+					activeProps={menuItem.to ? activeLinkProps : undefined}
+				>
+					{menuItem.icon}
+					<span className={`hidden ${menuItem.textBreakpoint}:inline-block`}>{menuItem.text}</span>
+					<span className={`${menuItem.textBreakpoint}:hidden`}>&nbsp;</span>
+				</Link>
+			</NavigationMenuLink>
+		</NavigationMenuItem>
+	);
+}
+
+function MobileNav({ menuItems }: { menuItems: Array<MenuGroup | MenuItem> }) {
+	const { user } = useOverallAuth();
+	const defaultCloudRoute = getDefaultSignedInCloudRouteForUser(user);
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const toggleMenu = useCallback(() => setIsMenuOpen(open => !open), []);
+	const closeMenu = useCallback(() => setIsMenuOpen(false), []);
+
+	return (
+		<div className="md:hidden" id="mobile-menu">
+			<div className="flex items-center justify-between">
+				<Link to={isLocalStudio ? defaultInstanceRoute : defaultCloudRoute}>
+					<Logo />
+					<Version />
+				</Link>
+				<button
+					type="button"
+					className="shadow-xs text-grey-400 hover:text-white hover:bg-black-dark"
+					onClick={toggleMenu}
+				>
+					<span className="sr-only">{isMenuOpen ? 'Close menu' : 'Open menu'}</span>
+					{isMenuOpen ? <X /> : <Menu />}
+				</button>
+			</div>
+			<div className={`${isMenuOpen ? 'fixed' : 'hidden'} top-40 bottom-0 left-0 right-0 bg-black-dark opacity-70`} onClick={closeMenu}></div>
+			<div
+				className={`${
+					isMenuOpen ? 'block' : 'hidden'
+				} md:hidden z-50 space-y-1 pb-3 bg-black-dark absolute left-0 top-full w-full rounded-b-md`}
+			>
+				{menuItems.map(menuItem => isMenuGroup(menuItem)
+					? !!menuItem.items.length && (
+					<div key={menuItem.text} className="bg-black pl-10 pr-2 py-2">
+						{menuItem.items.map(innerMenuItem => (
+							<MobileNavItem key={innerMenuItem.text} menuItem={innerMenuItem} onClick={closeMenu} />
+						))}
+					</div>
+				)
+					: (
+						<MobileNavItem key={menuItem.text} menuItem={menuItem} onClick={closeMenu} />
+					))}
+			</div>
+		</div>
+	);
+}
+
+function MobileNavItem({ menuItem, onClick }: { menuItem: MenuItem, onClick: () => void }) {
+	const linkOnClick = useCallback(() => {
+		menuItem.onClick?.();
+		onClick();
+	}, [menuItem, onClick]);
+	return (
+		<Link
+			to={menuItem.to}
+			onClick={linkOnClick}
+			className="flex flex-row px-3 py-2 text-base font-medium rounded-md text-gray-400 hover:text-white"
+			activeProps={menuItem.to ? activeLinkProps : undefined}
+		>
+			{menuItem.icon}
+			<span className="ml-4">
+				{menuItem.text}
+			</span>
+		</Link>
+	);
+}
+
 function Logo() {
 	return (
 		<>
@@ -281,37 +297,6 @@ function Logo() {
 				<img src="/harper-fabric_white.png" alt="Harper Fabric" className="w-64 hidden md:inline-block" />
 			)}
 			<img src="/HDBDogOnly.svg" width="50px" height="44px" alt="Harper" className="inline-block md:hidden" />
-		</>
-	);
-}
-
-export function Navbar() {
-	const { mutate: signOut } = useLogoutMutation();
-	const navigate = useNavigate();
-	const { user } = useOverallAuth();
-	const router = useRouter();
-	const handleSignOut = useCallback(() => {
-		signOut(undefined, {
-			onSuccess: async () => {
-				toast.success('Success', {
-					description: 'You have been signed out successfully.',
-					action: {
-						label: 'Dismiss',
-						onClick: () => toast.dismiss(),
-					},
-				});
-				void navigate({ to: '/sign-in' });
-				void router.invalidate();
-			},
-		});
-	}, [signOut, router, navigate]);
-	if (!user) {
-		return <AnonymousNav />;
-	}
-	return (
-		<>
-			<MobileNav signOut={handleSignOut} />
-			<DesktopNav signOut={handleSignOut} />
 		</>
 	);
 }
