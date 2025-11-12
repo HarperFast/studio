871ab01025143076a8bb44a245963359e7fbb477
Dawson Toth <dawson@harperdb.io>
2025-11-12 12:30:01 -0500

    fix: Parse more README links and add coverage

--- src/features/instance/applications/lib/parseReadMe.ts
+++ src/features/instance/applications/lib/parseReadMe.ts
@@ -1,10 +1,10 @@
 import { GetComponentFileResponse } from '@/features/instance/operations/queries/getComponentFile';
 
-export function parseReadMe(contents: string, baseURL: string, response: GetComponentFileResponse): string {
+export function parseReadMe(contents: string, baseURL: string, response: Pick<GetComponentFileResponse, 'project'>): string {
 	const operations9925URL = baseURL;
-	const rest9926URL = baseURL.replace(':9925', '');
+	const rest9926URL = baseURL.replace(/:9925\/?/, '');
 	if (operations9925URL) {
-		contents = contents.replaceAll('http://localhost:9926', rest9926URL);
+		contents = contents.replaceAll(/https?:\/\/localhost:9926/g, rest9926URL);
 	}
 	if (response.project) {
 		contents = contents.replaceAll('Your New Harper Fabric App', response.project);
