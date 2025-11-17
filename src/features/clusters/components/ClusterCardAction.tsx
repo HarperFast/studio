75926a5b155c420e7568602ce2c66562ca0fcbe6
Dawson Toth <dawson@harperdb.io>
2025-11-17 17:23:54 -0500

    feat: Connect vs Direct Sign In Buttons
    These will only show up for useres with update permissions on a given cluster or org. We’ll implement the actual backing functionality next.
    
    https://harperdb.atlassian.net/browse/STUDIO-583

--- src/features/clusters/components/ClusterCardAction.tsx
+++ src/features/clusters/components/ClusterCardAction.tsx
@@ -3,14 +3,14 @@ import { defaultInstanceRoute } from '@/config/constants';
 import { useInstanceAuth } from '@/hooks/useAuth';
 import { useOrganizationClusterPermissions } from '@/hooks/usePermissions';
 import { Cluster } from '@/lib/api.patch';
+import { clusterIsSelfManaged } from '@/lib/api/clusterIsSelfManaged';
 import { Link } from '@tanstack/react-router';
 import { ArrowRight } from 'lucide-react';
-import { useMemo } from 'react';
 
 export function ClusterCardAction({ cluster }: { cluster: Cluster }) {
 	const auth = useInstanceAuth(cluster.id);
 	const { view, update } = useOrganizationClusterPermissions(cluster.organizationId, cluster.id);
-	const isPendingResetPassword = useMemo(() => cluster.resetPassword, [cluster]);
+
 	if (!view) {
 		return undefined;
 	}
@@ -23,7 +23,7 @@ export function ClusterCardAction({ cluster }: { cluster: Cluster }) {
 		</Link>;
 	}
 
-	if (isPendingResetPassword) {
+	if (cluster.resetPassword) {
 		if (update) {
 			return <Link to={`/${cluster.organizationId}/${cluster.id}/finish-setup`} className="text-sm text-nowrap" aria-label={`Set Password on ${cluster.name}`} title={`Set Password on ${cluster.name}`}>
 				<Button variant="positive" className="py-2 hover:border-b-2 animate-glow-pulse">
@@ -39,16 +39,26 @@ export function ClusterCardAction({ cluster }: { cluster: Cluster }) {
 	if (auth.isLoading) {
 		return undefined;
 	}
+
 	if (auth.user) {
 		return <Link to={`/${cluster.organizationId}/${cluster.id}${defaultInstanceRoute}`} className="text-sm text-nowrap" aria-label={`View ${cluster.name}`} title={`View ${cluster.name}`}>
 			<span className="py-2 hover:border-b-2">
-				View <ArrowRight className="inline-block" />
+				Direct Connect <ArrowRight className="inline-block" />
 			</span>
 		</Link>;
 	}
+
+	if (update && !clusterIsSelfManaged(cluster)) {
+		return <Link to={`/${cluster.organizationId}/${cluster.id}${defaultInstanceRoute}`} className="text-sm text-nowrap" aria-label={`Connect to ${cluster.name}`} title={`Connect to ${cluster.name}`}>
+			<span className="py-2 hover:border-b-2">
+				Fabric Connect <ArrowRight className="inline-block" />
+			</span>
+		</Link>;
+	}
+
 	return <Link to={`/${cluster.organizationId}/${cluster.id}/sign-in`} className="text-sm text-nowrap" aria-label={`Sign In to ${cluster.name}`} title={`Sign In to ${cluster.name}`}>
 		<span className="py-2 hover:border-b-2">
-			Sign In <ArrowRight className="inline-block" />
+			Direct Sign In <ArrowRight className="inline-block" />
 		</span>
 	</Link>;
 }
