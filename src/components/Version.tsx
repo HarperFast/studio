26a6ec36c6366facd601a424897e5b82efc32713
Dawson Toth <dawson@harperdb.io>
2025-11-17 14:49:47 -0500

    feat: Link to releases
    https://harperdb.atlassian.net/browse/STUDIO-593

--- src/components/Version.tsx
+++ src/components/Version.tsx
@@ -2,9 +2,8 @@ import { badgeVariants } from '@/components/ui/badgeVariants';
 import { cn } from '@/lib/cn';
 
 export function Version() {
-	return (<>
-		<span
-			className={cn(badgeVariants({ variant: 'default' }), 'text-xs inline-block ml-2 align-text-top')}
-		>{import.meta.env.VITE_STUDIO_VERSION} BETA</span>
-	</>);
+	return (<a href="https://github.com/HarperFast/studio/releases" target="_blank" rel="noopener noreferrer">
+		<span className={cn(badgeVariants({ variant: 'default' }), 'text-xs inline-block ml-2 align-text-top')}>
+			{import.meta.env.VITE_STUDIO_VERSION} BETA</span>
+	</a>);
 }
