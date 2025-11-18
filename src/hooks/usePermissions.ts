c046307b8c6536f82b3da29920ac8588c4f97b69
Dawson Toth <dawson@harperdb.io>
2025-11-18 12:40:46 -0500

    chore: Extract non-hook versions of several permissions
    https://harperdb.atlassian.net/browse/STUDIO-583

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -5,10 +5,23 @@ import {
 	LocalRoleAttributePermissionAction,
 	LocalRolePermissionAction,
 	LocalRolePermissionTable,
+	User,
 } from '@/lib/api.patch';
 import { useParams } from '@tanstack/react-router';
 
-export function useOrganizationPermissions(orgId?: string): { update: boolean; remove: boolean; } {
+interface UR {
+	update: boolean;
+	remove: boolean;
+}
+
+interface CRUV {
+	create: boolean;
+	remove: boolean;
+	update: boolean;
+	view: boolean;
+}
+
+export function useOrganizationPermissions(orgId?: string): UR {
 	const { user } = useCloudAuth();
 	const { organizationId: orgIdFromRoute }: { organizationId: string; } = useParams({ strict: false });
 	const role = user?.roles?.[orgId ?? orgIdFromRoute];
@@ -21,12 +34,7 @@ export function useOrganizationPermissions(orgId?: string): { update: boolean; r
 	return { update: role.organization.update, remove: role.organization.delete };
 }
 
-export function useOrganizationRolePermissions(orgId?: string): {
-	create: boolean;
-	remove: boolean;
-	update: boolean;
-	view: boolean;
-} {
+export function useOrganizationRolePermissions(orgId?: string): CRUV {
 	const { user } = useCloudAuth();
 	const { organizationId: orgIdFromRoute }: { organizationId: string; } = useParams({ strict: false });
 	const role = user?.roles?.[orgId ?? orgIdFromRoute];
@@ -40,26 +48,29 @@ export function useOrganizationRolePermissions(orgId?: string): {
 	return { create: roles.create, remove: roles.delete, update: roles.update, view: roles.view };
 }
 
-export function useOrganizationClusterPermissions(orgId?: string, clusterId?: string): {
-	create: boolean;
-	remove: boolean;
-	update: boolean;
-	view: boolean;
-} {
+export function useOrganizationClusterPermissions(orgId?: string, clusterId?: string): CRUV {
 	const { user } = useCloudAuth();
 	const { organizationId: orgIdFromRoute, clusterId: clusterIdFromRoute }: {
 		organizationId: string;
 		clusterId: string;
 	} = useParams({ strict: false });
-	const findClusterId = clusterId ?? clusterIdFromRoute;
-	const role = user?.roles?.[orgId ?? orgIdFromRoute];
+
+	return getOrganizationClusterPermissions(
+		user,
+		orgId ?? orgIdFromRoute,
+		clusterId ?? clusterIdFromRoute,
+	);
+}
+
+export function getOrganizationClusterPermissions(user: User | null, orgId: string, clusterId: string): CRUV {
+	const role = user?.roles?.[orgId];
 	if (!role?.permission && !role?.organization?.clusters) {
 		return { create: false, remove: false, update: false, view: false };
 	}
 	if (role.permission?.super_user) {
 		return { create: true, remove: true, update: true, view: true };
 	}
-	const specificRoles = role.organization.clusters.resources?.find(r => r.id === findClusterId);
+	const specificRoles = role.organization.clusters.resources?.find(r => r.id === clusterId);
 	const genericRoles = role.organization.clusters;
 	return {
 		create: genericRoles.create,
@@ -69,19 +80,22 @@ export function useOrganizationClusterPermissions(orgId?: string, clusterId?: st
 	};
 }
 
-export function useOrganizationClusterInstancePermissions(orgId?: string, clusterId?: string): {
-	create: boolean;
-	remove: boolean;
-	update: boolean;
-	view: boolean;
-} {
+export function useOrganizationClusterInstancePermissions(orgId?: string, clusterId?: string): CRUV {
 	const { user } = useCloudAuth();
 	const { organizationId: orgIdFromRoute, clusterId: clusterIdFromRoute }: {
 		organizationId: string;
 		clusterId: string;
 	} = useParams({ strict: false });
-	const findClusterId = clusterId ?? clusterIdFromRoute;
-	const role = user?.roles?.[orgId ?? orgIdFromRoute];
+
+	return getOrganizationClusterInstancePermissions(
+		user,
+		orgId ?? orgIdFromRoute,
+		clusterId ?? clusterIdFromRoute,
+	);
+}
+
+export function getOrganizationClusterInstancePermissions(user: User | null, orgId: string, clusterId: string): CRUV {
+	const role = user?.roles?.[orgId];
 	if (!role?.permission && !role?.organization?.clusters) {
 		return { create: false, remove: false, update: false, view: false };
 	}
@@ -89,7 +103,7 @@ export function useOrganizationClusterInstancePermissions(orgId?: string, cluste
 		return { create: true, remove: true, update: true, view: true };
 	}
 	const specificRoles = role.organization.clusters
-		.resources?.find(r => r.id === findClusterId)
+		.resources?.find(r => r.id === clusterId)
 		?.instances;
 	const genericRoles = role.organization.clusters;
 	return {
