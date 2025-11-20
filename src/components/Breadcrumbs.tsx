f1b0db4293cf8307c9e0baad57d6c16ad6bbd2e6
Dawson Toth <dawson@harperdb.io>
2025-11-20 13:06:08 -0500

    feat: Show restart button in the top bar
    This reverts the revert commit 91b65341af47dd6f8b6d2ef6952d9c39070f9fa3.
    
    https://harperdb.atlassian.net/browse/STUDIO-358

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,12 +1,21 @@
+import { RestartButton } from '@/components/RestartButton';
+import { isLocalStudio } from '@/config/constants';
+import { useInstanceClientIdParams } from '@/config/useInstanceClient';
 import { Cluster, Instance, Organization } from '@/lib/api.patch';
 import { capitalizeWords } from '@/lib/string/capitalizeWords';
-import { Link, useLocation, useRouteContext } from '@tanstack/react-router';
+import { Link, useLocation, useParams, useRouteContext } from '@tanstack/react-router';
 import { HomeIcon } from 'lucide-react';
 import { useMemo } from 'react';
 
-export function Breadcrumbs() {
+export function Breadcrumbs({ restartRequired }: { restartRequired?: boolean }) {
 	const location = useLocation();
-	const { organization, instance, cluster }: { organization?: Organization; instance?: Instance; cluster?: Cluster } = useRouteContext({ strict: false });
+
+	const { organization, instance, cluster }: {
+		organization?: Organization;
+		instance?: Instance;
+		cluster?: Cluster
+	} = useRouteContext({ strict: false });
+
 	const breadcrumbs = useMemo(() => {
 		const routeHistory = location.pathname.split('/')
 			.filter((x) => x && x.length > 0);
@@ -65,8 +74,24 @@ export function Breadcrumbs() {
 	}, [location.pathname, cluster?.name, instance?.name, organization?.name]);
 
 	return (
-		<div role="list" className="flex items-center space-x-0 lg:space-x-2 xl:space-x-4 sm:max-w-9/10 max-w-[calc(100%-56px)]">
+		<div role="list" className="flex items-center space-x-2 xl:space-x-4 sm:max-w-9/10 max-w-[calc(100%-56px)]">
 			{...breadcrumbs}
+			{restartRequired && <BreadcrumbsRestartButton />}
 		</div>
 	);
+}
+
+function BreadcrumbsRestartButton() {
+	const { instanceId }: { instanceId?: string; clusterId: string; } = useParams({ strict: false });
+	const targetNoun = (instanceId || isLocalStudio) ? 'Instance' : 'Cluster';
+	const instanceParams = useInstanceClientIdParams();
+
+	return <RestartButton
+		className="animate-glow-pulse"
+		targetNoun={targetNoun}
+		instanceClient={instanceParams.instanceClient}
+		operation="restart"
+		hideText={true}
+		tooltip={`This ${targetNoun} is requesting a restart, when convenient, to apply your latest changes.`}
+	/>;
 }
