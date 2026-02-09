f805ca2fd98d762599d0d6a2c125e94b2fad8829
Dawson Toth <dawson@harperdb.io>
2026-02-09 11:20:53 -0500

    fix: Underline discord links
    https://harperdb.atlassian.net/browse/STUDIO-661

--- src/features/cluster/Scaling.tsx
+++ src/features/cluster/Scaling.tsx
@@ -51,8 +51,15 @@ export function Scaling() {
 					safely.{' '}
 					<span className="text-muted-foreground">
 						We will let you know when we are ready for you to connect! In the meantime, join us on{' '}
-						<a href="https://discord.gg/VzZuaw3Xay" target="_blank" rel="noreferrer">Discord</a>! Get real-time help
-						from our engineers, see feature drops early, and connect with others building on Fabric.
+						<a
+							href="https://discord.gg/VzZuaw3Xay"
+							target="_blank"
+							rel="noreferrer"
+							className="underline hover:text-blue-300"
+						>
+							Discord
+						</a>! Get real-time help from our engineers, see feature drops early, and connect with others building on
+						Fabric.
 					</span>
 				</p>
 			</div>
