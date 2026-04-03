6cc4cb9aeafb7dbb939d0595bd440ab5b554e555
Dawson Toth <dawson@harperdb.io>
2026-04-03 13:23:10 -0400

    fix: Proxy comment

--- src/integrations/datadog/datadog.ts
+++ src/integrations/datadog/datadog.ts
@@ -22,7 +22,7 @@ export function useDatadog() {
 				applicationId: 'f590deee-4bac-49b4-a202-3b6963d9721d',
 				clientToken: 'pub27aa29cea521492f4fef73032f86a023',
 
-				proxy: ({ path, parameters }) => `/Drink/?pour=${btoa(`${path}?${parameters}`)}`,
+				// proxy: ({ path, parameters }) => `/Drink/?pour=${btoa(`${path}?${parameters}`)}`,
 
 				site: 'datadoghq.com',
 				service: 'studio',
