06d86f112478ff4fb717782427310c1dfa6f18d9
Dawson Toth <dawson@harperdb.io>
2026-04-22 10:01:06 -0400

    feat: Allow hiding devtools during development

--- src/AppRouted.tsx
+++ src/AppRouted.tsx
@@ -9,7 +9,7 @@ export function AppRouted() {
 	return (
 		<>
 			<RouterProvider router={router} context={{ authentication }} />
-			{import.meta.env.DEV && <TanStackRouterDevtools router={router} position="bottom-right" />}
+			{!import.meta.env.VITE_DISABLE_DEVTOOLS && <TanStackRouterDevtools router={router} position="bottom-right" />}
 		</>
 	);
 }
